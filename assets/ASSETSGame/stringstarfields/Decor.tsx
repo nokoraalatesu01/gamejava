07ddb4a6399c7429c98c71df4193755f505b3667
Nguyễn Đức Trường <107637894+nokoraalatesu01@users.noreply.github.com>
2026-03-26 14:49:18 +0700

    Add files via upload

---
+++ assets/ASSETSGame/stringstarfields/Decor.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.2" name="Decor" tilewidth="16" tileheight="16" tilecount="884" columns="26">
+ <image source="../GandalfHardcore FREE Platformer Assets/GandalfHardcore FREE Platformer Assets/Decor.png" width="416" height="544"/>
+</tileset>
---
+++ assets/ASSETSGame/stringstarfields/House Tiles.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.2" name="House Tiles" tilewidth="16" tileheight="16" tilecount="392" columns="28">
+ <image source="../GandalfHardcore FREE Platformer Assets/GandalfHardcore FREE Platformer Assets/House Tiles.png" width="448" height="224"/>
+</tileset>
